a937f4d6ce1fbd972c6bba7fff5b4c75da440eb3
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-09-25 19:09:09 +0545

    admin panel

--- board/models.py
+++ board/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=200)
+    last_update = models.DateField(auto_now_add=True)
+    board = models.ForeignKey(Board,on_delete=models.CASCADE,related_name='topic')
+    starter = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user')
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(User,on_delete=models.CASCADE, null=True, related_name='+')
+
+    def __str__(self):
+        return self.message
--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
