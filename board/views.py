41fdb637c53947c9618ad6a1621b9ad8c5512f7e
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-10-03 08:33:53 +0545

    error removed commit

--- board/views.py
+++ board/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Board,Topic,Post
+from .models import Board, Topic, Post
 from django.contrib.auth.models import User
-
+from .forms import NewTopicForm
 
 # Create your views here.
+
+
 def index(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
@@ -18,29 +20,33 @@ def board_topics(request, id):
     return render(request, 'topics.html', {'board': board})
 
 
-def new_topics(request,id):
-    
+def new_topics(request, id):
+
     board = get_object_or_404(Board, pk=id)
-   
+    user = User.objects.first()
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
 
-        user = User.objects.first()
+        if form.is_valid():
+            subject = request.POST['subject']
+            message = request.POST['message']
 
-        topic = Topic.objects.create(
-            subject = subject,
-            board = board,
-            starter= user,
-        )
+            topic = Topic.objects.create(
+                subject=subject,
+                board=board,
+                starter=user
+            )
 
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
+            post = Post.objects.create(
+                message=message,
+                topic=topic,
+                created_by=user
+            )
 
-        return redirect('board_topics', id=board.id)
+            # TODO: redirect to the created topic page
+            return redirect('board_topics', id=board.id)
 
-    return render(request, 'new_topics.html', {'board': board})
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topics.html', {'board': board,'form':form})
