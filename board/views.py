57a164f38644a59362f10ab778327383654d4a1d
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-11-04 19:10:29 +0545

    form complete

--- board/views.py
+++ board/views.py
@@ -19,25 +19,20 @@ def board_topics(request, pk):
 
 def new_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first() 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
-
-    return render(request, 'new_topics.html', {'board': board})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk) 
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topics.html', {'board': board, 'form': form})
